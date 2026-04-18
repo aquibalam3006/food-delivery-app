64f639f766d565e3c58454a367ffeb9a60047341
AQUIB ALAM <mdaquib3006@gmail.com>
2026-04-18 16:20:59 +0530

    .env configured

--- PrestigeBackend/config/db.js
+++ PrestigeBackend/config/db.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
-
+import dotenv from "dotenv"
+dotenv.config();
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://mdaquib3006:78707560@cluster0.ecuubsw.mongodb.net/fooddel");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("DB connected ✅");
   } catch (error) {
     console.log("DB error ❌", error.message);
