db498c18b6b1d761bfc9cffa3a29aa501cfb6bc4
AQUIB ALAM <mdaquib3006@gmail.com>
2026-04-19 20:15:33 +0530

    fixed react router dom duplicate

--- PrestigePlates/src/pages/Cart/Cart.jsx
+++ PrestigePlates/src/pages/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom"; import React from 'react'
+import React from 'react'
 import './Cart.css'
 import { useContext } from 'react'
 import { StoreContext } from '../../context/StoreContext'
