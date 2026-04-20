0e245b2217c1c2a7326b964da464b740b8237ca5
AQUIB ALAM <mdaquib3006@gmail.com>
2026-04-20 15:17:26 +0530

    fixed cors

--- PrestigeBackend/server.js
+++ PrestigeBackend/server.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express"
 import cors from "cors"
-import { connectDB }from  "./config/db.js"
+import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import cartRouter from "./routes/cartRoute.js"
@@ -16,19 +16,26 @@ const port = 4000
 // middleware
 app.use(express.json())
 app.use(cors({
-  origin: "*"
+    origin: [
+        "http://localhost:5173",
+        "https://food-delivery-app-murex-one.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization", "token"],
+    credentials: true
 }));
+app.options("*", cors());
 
 // DB connection
 connectDB();
 
 
 //API end point
-app.use("/api/food",foodRouter)
-app.use("/images",express.static('uploads'))
-app.use("/api/user",userRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
+app.use("/api/food", foodRouter)
+app.use("/images", express.static('uploads'))
+app.use("/api/user", userRouter)
+app.use("/api/cart", cartRouter)
+app.use("/api/order", orderRouter)
 
 
 app.get("/", (req, res) => {
